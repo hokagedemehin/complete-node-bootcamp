fc0de163b52b36977434e5cd7fcd1c09a5c7a555
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-09-24 14:37:01 +0100

    Now installing packages

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const PORT = 8000;
 
@@ -9,16 +10,47 @@ const data = fs.readFileSync(
   path.join(__dirname, "dev-data", "data.json"),
   "utf-8"
 );
+
+const tempOverview = fs.readFileSync(
+  path.join(__dirname, "templates", "template-overview.html"),
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  path.join(__dirname, "templates", "template-card.html"),
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  path.join(__dirname, "templates", "template-product.html"),
+  "utf-8"
+);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathName = req.url;
 
-  if (pathName === "/overview") {
-    return res.end("This is the overview page");
-  } else if (pathName === "/product") {
-    return res.end("This is the product page");
-  } else if (pathName === "/api") {
+  if (pathname === "/overview" || pathname === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const product = dataObj[query.id];
+
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-Type": "application/json",
     });
---
+++ 1-node-farm/starter/modules/replaceTemplate.js
@@ -0,0 +1,14 @@
+module.exports = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName); // /content/g => this means to search for the content globally and perform a certain action
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
